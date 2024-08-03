76892b446d93ae1224fb6ed1a60d752f3d577d31
Unsiakl <zero.gaia.nexus@gmail.com>
2024-08-03 22:19:45 +0800

    Rename Grammar Check to Spelling Check

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,57 @@
 document.addEventListener('DOMContentLoaded', function () {
-    document.querySelector('#grammarForm').addEventListener('submit', function (event) {
+    document.querySelector('#spellingForm').addEventListener('submit', function (event) {
         event.preventDefault();
-        checkGrammar();
+        checkSpelling();
         alert('Form berhasil dikirim!');
     });
 });
 
-function checkGrammar() {
+function checkSpelling() {
     var text = document.getElementById('textToCheck').value;
     var result = text;
 
-    // Perbaiki beberapa kesalahan grammar umum (Contoh sederhana)
-    var corrections = {
+    // Perbaiki beberapa kesalahan spelling umum
+    var spellingCorrections = {
         "teh": "the",
         "adn": "and",
         "recieve": "receive",
         "occured": "occurred",
         "seperate": "separate",
         "definately": "definitely",
-        "goverment": "government"
+        "goverment": "government",
+        "wierd": "weird",
+        "enviroment": "environment",
+        "tommorow": "tomorrow",
+        "becuase": "because",
+        "beleive": "believe",
+        "arguement": "argument"
     };
 
-    for (var error in corrections) {
+    for (var error in spellingCorrections) {
         var regex = new RegExp("\\b" + error + "\\b", "gi");
-        result = result.replace(regex, corrections[error]);
+        result = result.replace(regex, spellingCorrections[error]);
     }
 
-    document.getElementById('grammarResult').innerText = 'Hasil: ' + result;
+    // Perbaiki beberapa kesalahan grammar dasar
+    var grammarCorrections = {
+        "\\bI amn't\\b": "I am not",
+        "\\bHe don't\\b": "He doesn't",
+        "\\bShe don't\\b": "She doesn't",
+        "\\bThey doesn't\\b": "They don't",
+        "\\bA apple\\b": "An apple",
+        "\\bAn university\\b": "A university",
+        "\\bI has\\b": "I have",
+        "\\bYou was\\b": "You were",
+        "\\bThey was\\b": "They were",
+        "\\bI can able to\\b": "I can",
+        "\\bMore better\\b": "better",
+        "\\bMore worse\\b": "worse"
+    };
+
+    for (var error in grammarCorrections) {
+        var regex = new RegExp(error, "gi");
+        result = result.replace(regex, grammarCorrections[error]);
+    }
+
+    document.getElementById('spellingResult').innerText = 'Hasil: ' + result;
 }
